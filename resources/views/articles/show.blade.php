472a1ae5af1344d29b71706529cf947c75785aa0
yoshihub <saiga123.nm@gmail.com>
2022-05-22 19:58:43 +0900

    デザイン調整

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,35 +6,35 @@
 @include('nav')
 
 <div class="container">
-  <div class="row">
-    <div class="card bg-light mt-2">
-      <div class="card-body">
-        <img class="card-img-top subject" src="{{asset($article->img)}}" alt="教材の画像">
+  <div class="card bg-light mb-1 shadow-sm">
+    <div class="card-body row">
+      <img class="img-fluid col-sm-12 col-md-8 col-lg-5" src="{{asset($article->img)}}" alt="教材の画像">
+      <div class="col-lg-7">
         <h5 class="card-title fs-4 mt-2">{{$article->title}}
         </h5>
         <p class="card-text">
           {{$article->body}}
         </p>
-
-        @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-        @endif
-
-        <form method="POST" action="{{route('articles.store')}}">
-          @csrf
-          <textarea class="form-control border border-info border-1" type="text" name="comment" cols="30" rows="3"></textarea>
-          <input type="hidden" name="id" value="{{$article->id}}">
-          <button class="btn btn-success mt-2" type="submit">口コミ投稿</button>
-        </form>
       </div>
+      @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+      @endif
+
+      <form class="form-group mt-2" method="POST" action="{{route('articles.store')}}">
+        @csrf
+        <textarea class="form-control border border-info border-1" type="text" name="comment" cols="30" rows="3"></textarea>
+        <input type="hidden" name="id" value="{{$article->id}}">
+        <button class="btn btn-success mt-2" type="submit">口コミ投稿</button>
+      </form>
     </div>
   </div>
+
 
   <div>
     <h3>口コミ一覧</h3>
@@ -47,5 +47,5 @@
     </div>
     @endforeach
   </div>
-
-  @endsection
+</div>
+@endsection
--- resources/views/articles/subject.blade.php
+++ resources/views/articles/subject.blade.php
@@ -8,15 +8,17 @@
 <div class="container">
   <div class="row g-2">
     @foreach($articles as $article)
-    <div class="card bg-light mt-2">
+    <div class="card bg-light mb-1 shadow-sm">
       <div class="card-body row">
-        <img class="card-img-top subject" src="{{asset($article->img)}}" alt="教材の画像">
-        <h5 class="card-title fs-4">{{$article->title}}
-        </h5>
-        <p class="card-text">
-          {{$article->body}}
-        </p>
-        <a class="btn btn-primary col-3" href="/articles/show/{{$article->id}}">詳細表示</a>
+        <img class="img-fluid col-sm-12 col-md-9 col-lg-5" src="{{asset($article->img)}}" alt="教材の画像">
+        <div class="col-lg-7">
+          <h5 class="card-title fs-4 mt-2">{{$article->title}}
+          </h5>
+          <p class="card-text">
+            {{$article->body}}
+          </p>
+          <a class="btn btn-primary col-3" href="/articles/show/{{$article->id}}">詳細表示</a>
+        </div>
       </div>
     </div>
     @endforeach
